918d44a7cda3b66939e503fcd57fd955b292cb1b
HY. Kelvin Lee <hykelvinlee42@gmail.com>
2024-04-09 10:45:59 -0400

    implemented multiple product setup logic

--- setup.sh
+++ setup.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-sudo cp -rf deepcool-ak620-digital.service /lib/systemd/system/
-sudo cp -rf deepcool-ak620-digital-restart.service /lib/systemd/system/
-sudo cp -rf deepcool-ak620-digital.py /usr/bin/deepcool-ak620-digital.py
-sudo systemctl enable deepcool-ak620-digital.service
-sudo systemctl enable deepcool-ak620-digital-restart.service
-sudo systemctl start deepcool-ak620-digital.service
+declare -A PRODUCTS
+
+PRODUCTS=(
+    [ak620]="0x0002"
+    [ak500s]="0x0004"
+)
+
+if [ "$#" -ne 2 ]
+then
+    echo "Please provide valid product and chipset names."
+    exit 1
+fi
+
+PRODUCT=$1
+CHIPSET=$2
+
+sed -i "/PRODUCT_ID = 0/c\PRODUCT_ID = ${PRODUCTS[${PRODUCT}]}" deepcool-ak-series-digital.py
+sed -i "/CHIPSET = \"\"/c\CHIPSET = \"${CHIPSET}\"" deepcool-ak-series-digital.py
+
+sudo cp -f deepcool-ak-series-digital.service /lib/systemd/system/
+sudo cp -f deepcool-ak-series-digital-restart.service /lib/systemd/system/
+sudo cp -f deepcool-ak-series-digital.py /usr/bin/deepcool-ak-series-digital.py
+
+sudo systemctl enable deepcool-ak-series-digital.service
+sudo systemctl enable deepcool-ak-series-digital-restart.service
+sudo systemctl start deepcool-ak-series-digital.service
