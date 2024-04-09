25183b9658b79c1605af27c08b80e02aa49d388d
raghulkrishna <rrkraghulkrishna@gmail.com>
2024-04-09 13:04:35 +0200

    feat: updated readme for installation steps

--- README.md
+++ README.md
@@ -14,10 +14,39 @@ You can install by running the provided `setup.sh` script:
 ./setup.sh
 ```
 
+### Step-by-Step Guide
+
+1. **Install Python Dependencies**: First, you need to install the necessary Python libraries, `hidapi` and `psutil`. These libraries allow the script to interact with the hardware and monitor system resources.
+
+   Open a terminal and run the following commands:
+   ```bash
+   pip install hid
+   pip install psutil
+   ```
+    Note: If you encounter permission errors, try adding --user to install the packages for your user only or use sudo to install them system-wide (not recommended for pip).
+
+2. **Clone the Repository**:   The script and necessary configuration files are hosted on GitHub. Use git to clone the repository to your local machine.
+    ```bash
+    git clone https://github.com/raghulkrishna/deepcool-ak620-digital-linux
+   ```
+3. **Navigate to the Project Directory**: Change your current directory to the newly cloned project folder.
+
+    ```bash
+    cd deepcool-ak620-digital-linux
+    ```
+4. **Run the Setup Script**: : The setup.sh script will automate the configuration and setup process. Run the script by executing:
+
+    For coolers other than ak620 go to **deepcool-ak620-digital.py** and modify the **DEFAULT_VENDOR_ID** and **DEFAULT_PRODUCT_ID** accordingly. Check Trobuleshooting section on how to get vendor id and product id.
+
+    ```bash
+    bash ./setup.sh
+    ```    
+
 ## Troubleshooting
 
 1) If you encounter any errors related to HIDAPI or psutil, ensure that the dependencies are installed correctly by running the setup.sh script.
 2) Make sure the AK620 digital air cooler is properly connected to your system and that the correct Vendor ID and Product ID are set in the script.
+3) How to verify Product ID and Vendor ID ?  use lsusb -v to get the list of devices ans search for your cooler.
 
 Credits
 https://github.com/Algorithm0/deepcool-digital-info
--- deepcool-ak620-digital.py
+++ deepcool-ak620-digital.py
@@ -1,50 +1,69 @@
+import sys
 import time
 import hid
 import psutil
 
-VENDOR_ID = 0x3633  # DeepCool's Vendor ID
-PRODUCT_ID = 0x0002  # AK620's Product ID
+# Default values for Vendor ID and Product ID
+DEFAULT_VENDOR_ID = 0x3633  # DeepCool's Vendor ID
+DEFAULT_PRODUCT_ID = 0x0002  # AK620's Product ID
 INTERVAL = 2
+
+# Check for command-line arguments and assign them if provided
+VENDOR_ID = int(sys.argv[1], 0) if len(sys.argv) > 1 else DEFAULT_VENDOR_ID
+PRODUCT_ID = int(sys.argv[2], 0) if len(sys.argv) > 2 else DEFAULT_PRODUCT_ID
 
 def get_bar_value(input_value):
     return (input_value - 1) // 10 + 1
 
-def get_data(value=0,mode='util'):
-    base_data=[16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+def get_data(value=0, mode='util'):
+    base_data = [16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     numbers = [int(char) for char in str(value)]
-    base_data[2]=get_bar_value(value)
-    if  mode =='util':
-        base_data[1]=76
-    elif mode =='start':
-        base_data[1]=170
+    base_data[2] = get_bar_value(value)
+    if mode == 'util':
+        base_data[1] = 76
+    elif mode == 'start':
+        base_data[1] = 170
         return base_data
-    elif mode== 'temp':
-        base_data[1]=19
+    elif mode == 'temp':
+        base_data[1] = 19
     if len(numbers) == 1:
-        base_data[5]=numbers[0]
+        base_data[5] = numbers[0]
     elif len(numbers) == 2:
-        base_data[4]= numbers[0]
-        base_data[5]= numbers[1]
+        base_data[4] = numbers[0]
+        base_data[5] = numbers[1]
     elif len(numbers) == 3:
-        base_data[3]=numbers[0]
-        base_data[4]=numbers[1]
-        base_data[5]=numbers[2]
+        base_data[3] = numbers[0]
+        base_data[4] = numbers[1]
+        base_data[5] = numbers[2]
     elif len(numbers) == 4:
-        base_data[3]=numbers[0]
-        base_data[4]=numbers[1]
-        base_data[5]=numbers[2]
-        base_data[6]=numbers[3]
+        base_data[3] = numbers[0]
+        base_data[4] = numbers[1]
+        base_data[5] = numbers[2]
+        base_data[6] = numbers[3]
     return base_data
 
+def get_cpu_temperature(label="CPU"):
+    sensors = psutil.sensors_temperatures()
+    for sensor_label, sensor_list in sensors.items():
+        for sensor in sensor_list:
+            if sensor.label == label:
+                return sensor.current
+    return 0
+
 def get_temperature():
-    temp = round(psutil.sensors_temperatures()['nct6687'][0].current)
-    return get_data(value=temp,mode='temp')
+    try:
+        temp = round(psutil.sensors_temperatures()['nct6687'][0].current)
+    except KeyError:
+        temp = get_cpu_temperature()
+    return get_data(value=temp, mode='temp')
+
 def get_utils():
-    utils= round(psutil.cpu_percent())
-    return get_data(value=utils,mode='util')
+    utils = round(psutil.cpu_percent())
+    return get_data(value=utils, mode='util')
+
 try:
     h = hid.device()
-    h.open(VENDOR_ID, PRODUCT_ID)
+    h.open(VENDOR_ID, PRODUCT_ID)  # Use the dynamic values
     h.set_nonblocking(1)
     h.write(get_data(mode="start"))
     while True:
@@ -52,14 +71,15 @@ try:
         temp = get_temperature()
         h.write(temp)
         time.sleep(INTERVAL)
-        utils =get_utils()
+        utils = get_utils()
         h.write(utils)
         time.sleep(INTERVAL)
 except IOError as ex:
     print(ex)
     print("Ensure that the AK620 is connected and the script has the correct Vendor ID and Product ID.")
-    print("Update the VENDOR_ID and PRODUCT_ID variables in this script.")
+    print("Update the VENDOR_ID and PRODUCT_ID variables in this script or pass them as command-line arguments.")
 except KeyboardInterrupt:
     print("Script terminated by user.")
 finally:
-    h.close()
+    if 'h' in locals():
+        h.close()
